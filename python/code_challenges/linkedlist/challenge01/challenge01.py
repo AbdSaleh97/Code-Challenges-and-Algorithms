1bada21c2fc1a341ec083a57c8b861e99655e059
AbdSaleh97 <salehabd.897@gmail.com>
2024-05-19 19:24:19 +0300

    se

--- python/code_challenges/linkedlist/challenge01/challenge01.py
+++ python/code_challenges/linkedlist/challenge01/challenge01.py
@@ -1,10 +1,18 @@
-# Write here the code challenge solution
 class ListNode:
     """A node in a singly-linked list."""
     def __init__(self, val):
         """Initialize a ListNode with the given value."""
         self.val = val
         self.next = None
+
+    def __str__(self):
+        """Generate a string representation of the linked list starting from this node."""
+        current = self
+        values = []
+        while current is not None:
+            values.append(str(current.val))
+            current = current.next
+        return "->".join(values)
 
 def delete_node(node):
     """Delete the specified node from the linked list."""
