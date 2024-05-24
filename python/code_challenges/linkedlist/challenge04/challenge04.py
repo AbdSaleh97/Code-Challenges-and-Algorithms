ae30c193d7e879da1beef920a1ebd2a85f5151ea
AbdSaleh97 <salehabd.897@gmail.com>
2024-05-24 19:03:34 +0300

    ch04

---
+++ python/code_challenges/linkedlist/challenge04/challenge04.py
@@ -0,0 +1,28 @@
+from LinkedList import LinkedList
+
+class Ch04:
+    list = LinkedList()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+    list.append(4)
+    list.append(5)
+
+    list.print_linked_list()
+
+    list.reverse_linked_list()
+
+    list.print_linked_list()
+
+
+    list = LinkedList()
+    list.append(1)
+    list.append(2)
+    list.append(3)
+
+    list.print_linked_list()
+
+    list.reverse_linked_list()
+
+    list.print_linked_list()
+
